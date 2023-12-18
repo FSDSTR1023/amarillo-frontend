372890813c8c6a86c90103a52449a83a4bbd106c
tonebonedev <tonifcb@gmail.com>
2023-12-18 14:09:09 +0100

    managed to do the component for createPost so that it can be sent to the backend

--- clonetwitter/src/App.jsx
+++ clonetwitter/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Users from "./pages/Users";
+import Button from "./components/Button";
+import CreatePost from "./components/CreatePost";
+import EmailLogin from "./pages/EmailLogin";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      {<h1>Test</h1>}
+      {/* {<h1>Test</h1>} */}
       {<Users />}
+      {/* <Button />
+      <CreatePost /> */}
+      <EmailLogin />
+      <CreatePost />
+
       {/* <Navbar /> */}
       {/* <MainContainer>
       <Posts>
